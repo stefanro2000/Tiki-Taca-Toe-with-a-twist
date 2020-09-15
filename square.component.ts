6a24f8df1a9a94d355f0f117fc9f30ca304fda78
stefanro2000 <stefanro2000@gmail.com>
2020-09-14 20:22:46 -0400

    Add files via upload

---
+++ square.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <button nbButton *ngIf="!value"> {{ value }} </button>
+    <button nbButton hero status ="success" *ngIf="value =='X'"> {{ value }} </button>
+    <button nbButton hero status ="success" *ngIf="value =='O'"> {{ value }} </button>
+  `,
+  styles: [
+  ]
+})
+export class SquareComponent  {
+  @Input() value: 'X' | 'O';
+
+}
